6033ca57c41b54e769b5f839fab752b2b0695f05
SahilRout <sahilrout07@gmail.com>
2023-04-30 22:10:20 +0530

    port changed

--- server.js
+++ server.js
@@ -69,5 +69,5 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
