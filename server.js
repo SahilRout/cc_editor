f9de1c24e175f80a05b6db0e3c027daca7b2db81
SahilRout <sahilrout07@gmail.com>
2023-04-30 22:00:44 +0530

    added get

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const ACTIONS = require('./src/Actions');
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        message: "Hello World",
+    });
+});
 app.use(express.static('build'));
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
